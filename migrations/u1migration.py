46484799a61b0c027ba0c7f0a53c0109ac227a4f
Aidan Fitzpatrick <aidan@reincubate.com>
2018-06-17 20:37:03 +0100

    Added 1.0 unstable placeholder migration.

---
+++ migrations/u1migration.py
@@ -0,0 +1,8 @@
+from lxml import etree
+from os.path import join, dirname, normpath, exists, split
+
+from util.xml import insert_after
+
+
+def migrate( save, a18, new_save ):
+    raise NotImplementedError()
